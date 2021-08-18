4fc1bd382dab3c26c715a19fb65bc8849ee78255
david <davidqhr@gmail.com>
2021-08-18 22:48:29 +0800

    remove electron-devtools-installer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { app, BrowserWindow } from "electron";
-import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
 import * as isDev from "electron-is-dev";
 import * as path from "path";
 import { registerHandlers } from "./bridgeHandlers";
@@ -64,11 +63,6 @@ const runApp = () => {
   app.on("before-quit", () => {});
 
   app.whenReady().then(() => {
-    // DevTools
-    installExtension(REACT_DEVELOPER_TOOLS)
-      .then((name) => console.log(`Added Extension:  ${name}`))
-      .catch((err) => console.log("An error occurred: ", err));
-
     createWindow();
 
     app.on("activate", () => {
